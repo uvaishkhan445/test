c31bcfa2d469d132ad9579dc0d18247d933657d8
UvaishaKhan <uvaisha.khan@goolean.tech>
2024-05-16 18:24:02 +0530

    file changes

--- p.py
+++ p.py
@@ -1,27 +1,24 @@
+# importing required library
+import mysql.connector
 
-# importing required library 
-import mysql.connector 
-   
-# connecting to the database 
+# connecting to the database
 dataBase = mysql.connector.connect(
-                     host = "localhost",
-                     user = "root",
-                     passwd = "",
-                     database = "oyly" ) 
-   
-# preparing a cursor object 
+    host="localhost", user="root", passwd="", database="oyly"
+)
+
+# preparing a cursor object
 cursorObject = dataBase.cursor()
-   
+
 print("Displaying NAME and ROLL columns from the STUDENT table:")
- 
+
 # selecting query
 query = "SELECT * FROM contact_us"
 cursorObject.execute(query)
- 
+
 myresult = cursorObject.fetchall()
- 
+
 for x in myresult:
     print(x)
- 
+
 # disconnecting from server
 dataBase.close()
