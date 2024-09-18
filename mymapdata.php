e54777a7d8dac793f0b389684065d90f2d3bbe2c
UvaishaKhan <uvaisha.khan@goolean.tech>
2024-09-18 16:47:42 +0530

    fetch with form

--- mymapdata.php
+++ mymapdata.php
@@ -1,2 +1,4 @@
 <?php
-print_r($_POST);
+header("Access-Control-Allow-Origin: *"); // Allow any origin for CORS
+header("Content-Type: application/json"); // Ensure correct content type
+echo json_encode($_POST);
